2cd709ee81baf0340747d5444b87c21cf0ba0e1d
mrugeshparekh <mrugeshparekh@gmail.com>
2015-08-24 15:33:42 -0400

    type write with multiple string

--- js/typewriter.js
+++ js/typewriter.js
@@ -1,6 +1,8 @@
 var caption = 'Improve your English to... (access more knowledge)';
 var captionLength = 0;
-
+var delay = 150;
+var count = 0;
+var msg = [ "(access more knowledge)", "(Frist String)", "(Second String)", "(Third String)", "(Fourth String)" ];
 
 $(document).ready(function(){
 
@@ -19,16 +21,29 @@ function cursorEffect(){
 }
 
 function type(){
-
+    
     $("#caption").html(caption.substr(0,captionLength++));
     if(captionLength < caption.length + 1){
-        setTimeout('type()', 100);
+        setTimeout('type()', delay);
     }
     else{
         captionLength = 0;
         testEarseEffect();
     }
+}
 
+function insertMultipleString(){
+    
+    while(count < 5){
+        if(count >= 4){
+            count = 0;
+            caption = 'Improve your English to... (access more knowledge)';
+            break;
+        }
+        caption = caption.replace(msg[count], msg[++count]);
+        break;
+    }
+    type();
 }
 
 function testEarseEffect(){
@@ -41,11 +56,13 @@ function testEarseEffect(){
 
 function earse(){
     $("#caption").html(caption.substr(0,captionLength--));
-    if( captionLength >= 0 ){
-        setTimeout('earse()', 100);
+    if( captionLength >= 27 ){
+        setTimeout('earse()', delay);
     }
     else{
-        captionLength = 0;
-        type();
+        //captionLength = 0;
+        captionLength = caption.indexOf(".");
+        captionLength = captionLength + 4;
+        insertMultipleString();
     }
 }
